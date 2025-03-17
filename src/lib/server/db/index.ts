c3e3901d4200fa3943c9583dad091632435a847a
BuSheeZy <6915170+busheezy@users.noreply.github.com>
2025-03-16 22:58:19 -0500

    Refactor database connection to use static environment variable

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,13 +1,13 @@
-import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/mysql2';
 import mysql from 'mysql2/promise';
 import * as schema from './schema';
+import { DATABASE_URL } from '$env/static/private';
 
-if (!env.DATABASE_URL) {
+if (!DATABASE_URL) {
 	throw new Error('DATABASE_URL is not set');
 }
 
-const client = await mysql.createConnection(env.DATABASE_URL);
+const client = await mysql.createConnection(DATABASE_URL);
 
 export const db = drizzle(client, {
 	schema,
