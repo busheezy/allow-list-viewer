497473a2a9f6c705dbaf50048337808b601c17e0
BuSheeZy <6915170+busheezy@users.noreply.github.com>
2025-03-16 22:46:11 -0500

    Add Steam integration and enhance allow list display
    - Integrated SteamAPI to fetch user summaries for allow list entries.
    - Added new dependencies: date-fns, steamapi, and steamid.
    - Updated allow list page to display Steam user avatars and links.
    - Improved HTML structure and styling for better user experience.

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,11 +1,37 @@
 import { db } from '$lib/server/db';
 import { allowListTable } from '$lib/server/db/schema';
+import SteamAPI from 'steamapi';
 import type { PageServerLoad } from './$types';
+import { STEAM_API_KEY } from '$env/static/private';
+import SteamID from 'steamid';
+import { desc } from 'drizzle-orm';
 
 export const load: PageServerLoad = async () => {
-	const allowList = await db.select().from(allowListTable);
+	const allowList = await db.select().from(allowListTable).orderBy(desc(allowListTable.added));
+	const steam = new SteamAPI(STEAM_API_KEY);
+
+	const steamIds = allowList.map((row) => {
+		const steamId = new SteamID(`STEAM_1:${row.steamId}`);
+
+		return steamId.getSteamID64();
+	});
+
+	const userSummaries = await steam.getUserSummary(steamIds);
+
+	if (!Array.isArray(userSummaries)) {
+		throw new Error('userSummaries is not an array');
+	}
+
+	const pojoUserSummaries = userSummaries.map((user) => {
+		return {
+			nickname: user.nickname,
+			avatar: user.avatar,
+			steamID: user.steamID
+		};
+	});
 
 	return {
-		allowList
+		allowList,
+		userSummaries: pojoUserSummaries
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,70 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
+	import SteamID from 'steamid';
+	import { formatDistanceToNow } from 'date-fns';
 
 	let { data }: PageProps = $props();
+
+	const { allowList, userSummaries } = data;
+
+	const allowListWithSteamSummary = allowList.map((player) => {
+		const steamId = new SteamID(`STEAM_0:${player.steamId}`);
+		const steamId64 = steamId.getSteamID64();
+
+		const userSummary = userSummaries.find((user) => user.steamID === steamId64);
+
+		return {
+			...player,
+			steam: userSummary,
+			steamLink: `https://steamcommunity.com/profiles/${steamId64}`,
+			kzProfieLink: `https://kzprofile.com/players/${steamId64}`,
+			steamId64
+		};
+	});
 </script>
 
 <h1 class="mb-4 text-3xl">Allow List</h1>
 
 <div>
-	<table class="table w-full table-fixed text-left">
+	<table class="table w-full text-left">
 		<thead class="text-2xl">
 			<tr>
-				<th>Name</th>
+				<th>&nbsp;</th>
+				<th>DB Name</th>
+				<th>Steam Name</th>
 				<th>Steam ID</th>
 				<th>Notes</th>
 				<th>Added</th>
+				<th>&nbsp;</th>
 			</tr>
 		</thead>
+
 		<tbody>
-			{#each data.allowList as player}
-				<tr>
+			{#each allowListWithSteamSummary as player}
+				<tr class="border-b border-gray-200">
+					<td class="mx-2">
+						<img src={player.steam?.avatar.large} alt="Avatar" class="h-12 w-12" />
+					</td>
 					<td>{player.name}</td>
-					<td>{player.steamId}</td>
+					<td>{player.steam?.nickname}</td>
+					<td>{player.steamId64}</td>
 					<td>{player.notes}</td>
-					<td>{player.added}</td>
+					<td>{player.added ? formatDistanceToNow(player.added) : 'N/A'} ago</td>
+					<td>
+						<a
+							href={player.steamLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="text-blue-500 hover:underline">Steam Profile</a
+						>
+
+						<a
+							href={player.kzProfieLink}
+							target="_blank"
+							rel="noopener noreferrer"
+							class="ml-2 text-blue-500 hover:underline">KZ Profile</a
+						>
+					</td>
 				</tr>
 			{/each}
 		</tbody>
