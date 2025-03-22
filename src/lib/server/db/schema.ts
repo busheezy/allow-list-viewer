8e1464fcda98463735e1d1d75a41077f1a275be5
BuSheeZy <6915170+busheezy@users.noreply.github.com>
2025-03-22 15:23:23 -0500

    autogen was weird

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -3,9 +3,9 @@ import { mysqlTable, text, timestamp, unique, varchar } from 'drizzle-orm/mysql-
 export const allowlistTable = mysqlTable(
 	'allowlist',
 	{
-		name: varchar({ length: 50 }).default('NULL'),
+		name: varchar({ length: 50 }),
 		steamId: varchar({ length: 50 }).notNull(),
-		notes: text().default('NULL'),
+		notes: text(),
 		added: timestamp({ mode: 'string' }).default('current_timestamp()')
 	},
 	(table) => [unique('steamid').on(table.steamId)]
